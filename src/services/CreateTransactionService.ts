4a4bf37b79d6dc47670d3186311fd74901d87656
Raul Neto <raulneto90@gmail.com>
2020-12-10 08:19:21 -0300

    Create transaction

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,55 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository, getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Category from '../models/Category';
 
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    type,
+    category,
+    value,
+  }: Request): Promise<Transaction> {
+    const categoriesRepository = getRepository(Category);
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (total <= value && type === 'outcome') {
+      throw new AppError('You cant add a transaction without balance');
+    }
+
+    let categoryExists = await categoriesRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryExists) {
+      const newCategory = categoriesRepository.create({
+        title: category,
+      });
+
+      categoryExists = await categoriesRepository.save(newCategory);
+    }
+
+    const transaction = transactionsRepository.create({
+      title,
+      type,
+      category: categoryExists,
+      value,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
